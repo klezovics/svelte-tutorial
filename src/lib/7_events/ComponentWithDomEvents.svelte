560359ba810ca76a7abe64fdd7ebf72436aae9c1
klezovich <arthur@trevor.io>
2023-05-29 15:43:18 +0200

    Add code, which shows how to work with events

---
+++ src/lib/7_events/ComponentWithDomEvents.svelte
@@ -0,0 +1,17 @@
+<script>
+    let m = { x: 0, y: 0 };
+
+    function handleMousemove(event) {
+        m.x = event.clientX;
+        m.y = event.clientY;
+    }
+</script>
+
+<div class="box" on:mousemove={handleMousemove}>
+    The mouse position is {m.x} x {m.y}
+</div>
+
+<style>
+    div { width: 100%; height: 100%; }
+    .box { background-color: #eee; padding: 1em; }
+</style>
