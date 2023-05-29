a813d35c986bfe3932cde0d61d4c055a68659de2
klezovich <arthur@trevor.io>
2023-05-29 16:05:27 +0200

    Add code examples for context API

---
+++ src/lib/14_context_api/ChildComponent.svelte
@@ -0,0 +1,19 @@
+<!-- Child.svelte -->
+<script>
+    import { getContext } from 'svelte';
+
+    // Get the sharedData store from the context
+    // For this to work, this component must be a child of a component which creates a store
+    const sharedData = getContext('sharedData');
+
+    // Update the shared data with a new value
+    function updateSharedData() {
+        sharedData.set('Updated shared data from Child component');
+    }
+</script>
+
+<section>
+    <h2>Child Component</h2>
+    <p>Shared Data: {$sharedData}</p>
+    <button on:click={updateSharedData}>Update Shared Data</button>
+</section>
---
+++ src/lib/14_context_api/ParentComponent.svelte
@@ -0,0 +1,16 @@
+<!-- App.svelte -->
+<script>
+    import { setContext } from 'svelte';
+    import { writable } from 'svelte/store';
+    import ChildComponent from "./ChildComponent.svelte";
+
+    // Create a writable store to hold the shared data
+    const sharedData = writable('Initial shared data');
+    // Set the sharedData store as the context value
+    setContext('sharedData', sharedData);
+</script>
+
+<main>
+    <h1>App Component</h1>
+    <ChildComponent />
+</main>
