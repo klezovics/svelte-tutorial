2125018e92384fd7facba711066019f826150c9f
klezovich <arthur@trevor.io>
2023-05-29 15:37:49 +0200

    Add slot props example

---
+++ src/lib/4_component_composition/2_slots/3_slot_props/ComponentWithSlotProps.svelte
@@ -0,0 +1,53 @@
+<script>
+    import Hoverable from './Hoverable.svelte';
+</script>
+
+<!-- let:hovering -> allows to pass a prop to the component-->
+<!--hovering -> property on the child component-->
+<!--active -> some virtual property on the parent component-->
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <p>I am being hovered upon.</p>
+        {:else}
+            <p>Hover over me!</p>
+        {/if}
+    </div>
+</Hoverable>
+
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <p>I am being hovered upon.</p>
+        {:else}
+            <p>Hover over me!</p>
+        {/if}
+    </div>
+</Hoverable>
+
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <p>I am being hovered upon.</p>
+        {:else}
+            <p>Hover over me!</p>
+        {/if}
+    </div>
+</Hoverable>
+
+<style>
+    div {
+        padding: 1em;
+        margin: 0 0 1em 0;
+        background-color: #eee;
+    }
+
+    .active {
+        background-color: #ff3e00;
+        color: white;
+    }
+
+    p {
+        pointer-events: none;
+    }
+</style>
