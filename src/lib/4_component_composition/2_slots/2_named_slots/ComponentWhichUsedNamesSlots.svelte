d4f9d1bf0cc66a25b1664558f52615dc320c10d7
klezovich <arthur@trevor.io>
2023-05-29 15:27:05 +0200

    Add code relate to slots

---
+++ src/lib/4_component_composition/2_slots/2_named_slots/ComponentWhichUsedNamesSlots.svelte
@@ -0,0 +1,25 @@
+<script>
+    import ContactCard from './ContactCard.svelte';
+</script>
+
+<ContactCard>
+	<span slot="name">
+		P. Sherman
+	</span>
+
+    <span slot="address">
+		42 Wallaby Way<br>
+		Sydney
+	</span>
+</ContactCard>
+
+<ContactCard>
+	<div slot="name">
+		Arthur K
+	</div>
+
+    <div slot="address">
+		Riga City. Fun stuff
+	</div>
+</ContactCard>
+
